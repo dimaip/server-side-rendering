d9ce281b88d142cf52861223a201de6d47dfd428
Dmitri Pisarev <dimaip@gmail.com>
2016-04-08 00:11:19 +0300

    Step 4a: asyncronously fetching data
    Now let's fetch some data asyncronously. We'll use isomorphic-fetch,
    as it works both on client and server our of the box.
    
    Fetching data works just fine on the server, but the problem is that
    on the server we didn't wait for fetch to finish fetching the data
    before sending the response, so our pre-rendered html doesn't have any
    async data when it arrives.
    Let's try to fix it in the next step.

--- Hello.js
+++ Hello.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import s from 'Hello.scss';
+import fetch from 'isomorphic-fetch';
 
 const Hello = React.createClass({
+  getInitialState: function() {
+    return {
+      hello: ''
+    };
+  },
+  componentDidMount: function() {
+    fetch('/static/data.json')
+      .then(r => r.json())
+      .then(r => this.setState({hello: r.hello}));
+  },
   render: function() {
-    return <div className={s.root}>Hello {this.props.name}</div>;
+    return <div className={s.root}>Hello {this.props.name}. Async hello {this.state.hello}</div>;
   }
 });
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 // Serve built files with express static files middleware
 app.use('/built', express.static(path.join(__dirname, 'built')));
 // Serve normal requests with our handleRender function
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.get('*', handleRender);
 app.listen(3000);
 console.log('=== Go to http://localhost:3000 ===');
